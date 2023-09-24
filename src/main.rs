111baf7c298bab73cb24798aeb5ae2b704170ce2
Clément Hamada <clementhamada@protonmail.com>
2023-09-24 12:30:57 +0200

    Update rust nightly and replace drain_filter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(extract_if)]
 use itertools::Itertools;
 use bitvec::prelude::*;
 
@@ -6,6 +7,7 @@ type Unop = fn(u32) -> u32;
 
 fn eval_formula(input: &str) -> bool {
 	let mut bits = bitvec![];
+
 	let mut chars = input.chars().peekable();
 	let operands = chars.peeking_take_while(|c| *c == '0' || *c == '1');
 
@@ -33,6 +35,41 @@ fn eval_formula(input: &str) -> bool {
 
 	bits.pop().expect("missing operation")
 }
+
+fn print_truth_row<'a>(variables: impl Iterator<Item = &'a char>, value: char) {
+	for variable in variables {
+		print!("| {} ", variable);
+	}
+	println!("| {} |", value);
+}
+
+fn print_truth_table(formula: &str) {
+	let mut chars: Vec<char> = formula.to_ascii_uppercase().chars().collect();
+	let variables: Vec<char> = chars.extract_if(|c| c.is_ascii_alphabetic()).collect();
+
+	let mut unique: Vec<char> = variables.clone();
+	unique.sort();
+	unique.dedup();
+
+	let variable_count = unique.len();
+
+	let mut rpn = String::from_iter(variables.iter());
+	rpn.extend(&chars);
+
+	dbg!(&rpn);
+	print_truth_row(unique.iter(), '=');
+
+	for mut combination in 0..1 << variable_count {
+		let mut substituted = rpn.clone();
+
+		for variable in unique.iter() {
+			let value = char::from_digit(combination & 1, 2).unwrap();
+			print!("| {} ", value);
+			substituted = substituted.replace(&variable.to_string(), &value.to_string());
+			combination >>= 1;
+		}
+		println!("| {} |", eval_formula(&substituted) as i32);
+	}
 }
 
 fn adder(a: u32, b: u32) -> u32 {
@@ -41,7 +78,7 @@ fn adder(a: u32, b: u32) -> u32 {
 	let mut carry;
 
 	loop {
-		carry = (a & b) << 1; 
+		carry = (a & b) << 1;
 
 		a ^= b;
 		b = a & carry;
@@ -51,7 +88,7 @@ fn adder(a: u32, b: u32) -> u32 {
 			break;
 		}
 	}
-	
+
 	a
 }
 
@@ -99,5 +136,10 @@ fn main() {
 	print_formula("110!&>");
 	print_formula("110&>");
 	print_formula("00=");
-	print_formula("");
+	print_formula("101|&");
+
+	print_truth_table("AB&");
+	print_truth_table("AB&C|");
+	print_truth_table("AB|C&");
+	print_truth_table("AB|B&");
 }
